5375b44b8f2354a95a0a9313d435d571d77c4f15
Paul Idorasi <paulbogdan97@gmail.com>
2020-11-23 17:08:40 +0200

    All-to-all: refactored validation

--- all-to-all/node.py
+++ all-to-all/node.py
@@ -7,36 +7,38 @@ from networknode import NetworkNode
 
 ips = ['10.128.0.2', '10.128.0.3', '10.128.0.5', '10.128.0.6', '10.128.0.7']
 nodes = []
+refreshed_nodes = []
 
 host = socket.gethostbyname(socket.gethostname())
-print(host)
-ips.remove(host)
+# ips.remove(host)
 
 def populate_node_list():
     for ip in ips:
         nodes.append(NetworkNode(ip))
-
+    refreshed_nodes = nodes
+    
 populate_node_list()
+
+def validateNodes():
+    while True:
+        nodes = refreshed_nodes
+        for node in nodes:
+            if node.failed == True:
+                print('Node with ip:' + node.ip + ' has failed')
+                refreshed_nodes.remove(node)
 
 def pingAll():
     while True:
         for node in nodes:
+            print('Pinging: ' + node.ip)
             if os.system('ping -c 1 ' + node.ip + ' >/dev/null') == 0:
-                print('Incrementing ' + node.ip)
                 node.incrementHb()
+            else:
+                node.markAsFailed()
+            validateNodes()
         time.sleep(2)
 
-def validateNodes():
-    time.sleep(5)
-    while True:
-        for node in nodes:
-            if node.failed == False and (datetime.now() - node.updateTime).total_seconds() > 10:
-                node.failed = True
-                print('Node with ip:' + node.ip + ' has failed')
-
-
-threading.Thread(target = pingAll).run()
-threading.Thread(target = validateNodes).run()
+pingAll()
             
 
         
